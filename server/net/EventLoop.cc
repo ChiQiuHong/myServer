d1c7a1fb6b83b7e42b372ee8a9bd744403ef04dd
YQ Huang <huangjiaershao@live.com>
2022-05-21 09:34:33 +0800

    fix EventLoop

--- server/net/EventLoop.cc
+++ server/net/EventLoop.cc
@@ -9,10 +9,9 @@
 
 #include "server/base/Logging.h"
 #include "server/base/Mutex.h"
-
+#include "server/net/Channel.h"
+#include "server/net/Poller.h"
 #include "server/net/SocketsOps.h"
-
-#include <poll.h>
 
 #include <algorithm>
 #include <signal.h>
@@ -25,6 +24,8 @@ namespace net {
 
 // 线程局部变量 指向当前线程内的EventLoop对象
 __thread EventLoop* t_loopInThisThread = 0;
+// 传递给epoll_wait timeout的参数 这是等待10秒
+const int kPollTimeMs = 10000;
 
 /**
  * 构造函数
@@ -53,25 +54,45 @@ EventLoop::~EventLoop() {
     t_loopInThisThread = NULL;
 }
 
-// 事件循环
+/**
+ * 定期检测是否有活跃的通道事件，有则处理，无则不处理
+ * 
+ */
 void EventLoop::loop() {
     assert(!looping_);
     assertInLoopThread();
     looping_ = true;
+    quit_ = false;
+    LOG_TRACE << "EventLoop" << this << " start looping";
 
-    ::poll(NULL, 0, 5*1000);
+    while(!quit_) {
+        activeChannels_.clear();
+        // 调用Poller::poll()获得当前活动事件的
+        pollReturnTime_ = poller_->poll(kPollTimeMs, &activeChannels_);
+        ++iteration_;
+        if(Logger::logLevel() <= Logger::TRACE) {
+            printActiveChannels();
+        }
+
+        // 然后依次调用每个Channel的handleEvent函数
+        eventHandling_ = true;
+        for(Channel* channel : activeChannels_) {
+            currentActiveChannel_ = channel;
+            currentActiveChannel_->handleEvent();
+        }
+        currentActiveChannel_ = NULL;
+        eventHandling_ = false;
+        // doPendingFunctors();
+    }
 
     LOG_TRACE << "EventLoop " << this << " stop looping";
     looping_ = false;
 }
 
-// //
-// void EventLoop::quit();
-
-// //
-// Timestamp EventLoop::pollReturnTime() const { return pollReturnTime_; }
-
-// int64_t EventLoop::iteration() const { return iteration_; }
+//
+void EventLoop::quit() {
+    quit_ = true;
+}
 
 // //
 // void EventLoop::runInLoop(Functor cb);
@@ -93,30 +114,82 @@ void EventLoop::loop() {
 
 // void EventLoop::cancel(TimerId timerId);
 
-// void EventLoop::wakeup();
-// void EventLoop::updateChannel(Channel* channel);
-// void EventLoop::removeChannel(Channel* cahnnel);
-// bool EventLoop::hasChannel(Channel* cahnnel);
+// 唤醒事件通知描述符
+void EventLoop::wakeup(){
+    uint64_t one = 1;
+    ssize_t n = sockets::write(wakeupFd_, &one, sizeof one);
+    if(n != sizeof one) {
+        LOG_ERROR << "EventLoop::wakeup() writes " << n << "bytes instead of 8";
+    }
+}
 
-// bool EventLoop::eventHanding() const { return eventHandling_; }
+/**
+ * updateChannel()在检查断言之后调用Poller::updateChannel()
+ * EventLoop不关心Poller是如何管理Channel列表的
+ */
+void EventLoop::updateChannel(Channel* channel) {
+    assert(channel->ownerLoop() == this);
+    assertInLoopThread();
+    poller_->updateChannel(channel);
+}
 
-// void EventLoop::setContext(const boost::any& context) { context_ = context; }
+// 删除事件分发器 调用Poller::removeChannel()
+void EventLoop::removeChannel(Channel* channel) {
+    assert(channel->ownerLoop() == this);
+    assertInLoopThread();
+    if(eventHandling_) {
+        assert(currentActiveChannel_ == channel ||
+            std::find(activeChannels_.begin(), activeChannels_.end(), channel) == activeChannels_.end());
+    }
+    poller_->removeChannel(channel);
+}
 
-// const boost::any& EventLoop::getContext() const { return context_; }
+// 判断 调用Poller::hasChannel()
+bool EventLoop::hasChannel(Channel* channel) {
+    assert(channel->ownerLoop() == this);
+    assertInLoopThread();
+    return poller_->hasChannel(channel);
+}
 
-// boost::any* EventLoop::getMutableContext() { return &context_; }
-
+// 返回当前线程内的EventLoop对象
 EventLoop* EventLoop::geteventLoopOfCurrentThread() {
     return t_loopInThisThread;
 }
 
-
+// 不在IO线程里
 void EventLoop::abortNotInLoopThread() {
     LOG_FATAL << "EventLoop::abortNotInLoopThread - EventLoop " << this
               << " was created in threadid_ = " << threadId_
               << ", current thread id = " << CurrentThread::tid(); 
 }
 
+// 执行转交给IO的任务
+void EventLoop::doPendingFunctors() {
+    std::vector<Functor> functors;
+    callingPendingFunctors_ = true;
+
+    // 这里使用了缩减临界区代码的技巧，减少锁的争用
+    // 相当于剪切操作
+    // 这里之所以会出现并发的问题，是因为本函数不会跨线程
+    // 但是runInLoop可以跨函数，会更改pendingFunctors_
+    // 所以这里需要加锁
+    {
+        MutexLockGuard lock(mutex_);
+        functors.swap(pendingFunctors_);
+    }
+
+    for(const Functor& functor : functors) {
+        functor();
+    }
+    callingPendingFunctors_ = false;
+}
+
+// DEBUG 将发生的事件写入日志
+void EventLoop::printActiveChannels() const {
+    for(const Channel* channel : activeChannels_) {  
+        LOG_TRACE << "{" << channel->reventsToString() << "} ";
+    }
+}
 
 }   // namespace net
 
--- server/net/EventLoop.h
+++ server/net/EventLoop.h
@@ -5,9 +5,12 @@
 * @date: 2022/05/14 09:32:26
 */
 
+#pragma once
+
 #include <atomic>
 #include <functional>
 #include <vector>
+#include <memory>
 
 #include <boost/any.hpp>
 
@@ -21,6 +24,14 @@ namespace myserver {
 
 namespace net {
 
+class Channel;
+class Poller;
+
+
+/**
+ * one loop per thread 每个线程最多只能有一个EventLoop对象
+ * 创建了EventLoop对象的线程称为IO线程，其功能是运行事件循环
+ */
 class EventLoop : noncopyable {
 public:
     typedef std::function<void()> Functor;
@@ -29,22 +40,22 @@ public:
     // 析构函数
     ~EventLoop();
 
-    // 
+    // 事件循环
     void loop();
 
-    // //
-    // void quit();
+    // 退出事件循环
+    void quit();
 
-    // //
-    // Timestamp pollReturnTime() const { return pollReturnTime_; }
-    
-    // int64_t iteration() const { return iteration_; }
+    // 返回poll return的时刻
+    Timestamp pollReturnTime() const { return pollReturnTime_; }
+    // 事件循环的次数
+    int64_t iteration() const { return iteration_; }
 
-    // //
-    // void runInLoop(Functor cb);
+    // 在主循环中运行
+    void runInLoop(Functor cb);
 
-    // //
-    // void queueInLoop(Functor cb);
+    // 插入主循环任务队列
+    void queueInLoop(Functor cb);
 
     // //
     // size_t queueSize() const;
@@ -60,58 +71,63 @@ public:
 
     // void cancel(TimerId timerId);
 
-    // void wakeup();
-    // void updateChannel(Channel* channel);
-    // void removeChannel(Channel* cahnnel);
-    // bool hasChannel(Channel* cahnnel);
+    void wakeup();                          // 唤醒事件通知描述符
+    void updateChannel(Channel* channel);   // 更新事件分发器
+    void removeChannel(Channel* channel);   // 删除事件分发器
+    bool hasChannel(Channel* channel);      // 判断
 
-    //
+    // 判断是否在当前线程运行
     void assertInLoopThread() {
         if(!isInLoopThread()) {
             abortNotInLoopThread();
         }
     }
 
-    //
+    // 判断是否在当前线程运行
     bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
 
-    // bool eventHanding() const { return eventHandling_; }
+    // 判断EventLoop是否在分发事件
+    bool eventHanding() const { return eventHandling_; }
 
+    // 
     // void setContext(const boost::any& context) { context_ = context; }
     
+    // //
     // const boost::any& getContext() const { return context_; }
 
+    // //
     // boost::any* getMutableContext() { return &context_; }
 
+    // 返回当前线程内的EventLoop对象
     static EventLoop* geteventLoopOfCurrentThread();
 
 private:
-    void abortNotInLoopThread();
-    // void handleRead();
-    // void doPendingFunctors();
+    void abortNotInLoopThread();    // 不在IO线程里
+    // void handleRead();           // 将事件通知描述符里的内容读走，以便让其检测事件通知
+    void doPendingFunctors();       // 执行转交给IO的任务
 
-    // void printActiveChannels() const;
+    void printActiveChannels() const;   // DEBUG 将发生的事件写入日志
 
-    // typedef std::vector<Channel*> ChannelList;
+    typedef std::vector<Channel*> ChannelList;  // 事件分发器列表
 
-    bool looping_;
-    // std::atomic<bool> quit_;
-    // bool eventHandling_;
-    // bool callingPendingFunctors_;
-    // int64_t iteration_;
-    const pid_t threadId_;
-    // Timestamp pollReturnTime_;
-    // std::unique_ptr<Poller> poller_;
-    // std::unique_ptr<TimerQueue> tiemrQueue_;
-    // int wakeupFd_;
-    // std::unique_ptr<Channel> wakeupChannel_;
-    // boost::any context_;
+    bool looping_;                      // 是否运行循环
+    std::atomic<bool> quit_;            // 是否退出事件循环
+    bool eventHandling_;                // EventLoop是否在分发事件
+    bool callingPendingFunctors_;       // EventLoop是否在处理任务
+    int64_t iteration_;                 // 事件循环的次数
+    const pid_t threadId_;              // 运行loop的线程ID
+    Timestamp pollReturnTime_;          // poll阻塞的时间
+    std::unique_ptr<Poller> poller_;    // IO复用
+    // std::unique_ptr<TimerQueue> tiemrQueue_; // 定时器列表
+    int wakeupFd_;                           // 唤醒套接字
+    // std::unique_ptr<Channel> wakeupChannel_; //
+    // boost::any context_;                     //
 
-    // ChannelList activeChannels_;
-    // Channel* currentActiveChannel_;
+    ChannelList activeChannels_;        // 活跃的事件列表
+    Channel* currentActiveChannel_;     // 当前处理的事件
 
-    // mutable MutexLock mutex_;
-    // std::vector<Functor> pendingFunctors_ GUARDED_BY(mutex_);
+    mutable MutexLock mutex_;           // 互斥锁
+    std::vector<Functor> pendingFunctors_;  // 需要在主I/O线程执行的任务
 };
 
 }   // namespace net
